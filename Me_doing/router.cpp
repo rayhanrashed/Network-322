01dc47288c51f6b5fb0c9163377d75bf7f65da7f
rayhanrashed <38178276+rayhanrashed@users.noreply.github.com>
2018-12-08 13:54:01 +0600

    Trying to Send. Kinda Successful
    No IDea

--- Me_doing/router.cpp
+++ Me_doing/router.cpp
@@ -42,6 +42,7 @@ char sending_buffer[1024];
 struct sockaddr_in receiving_address;
 struct sockaddr_in sending_address;
 struct sockaddr_in not_sure;
+struct sockaddr_in temporary_socket;
 socklen_t not_sure_len;
 char router_1_address[1024];
 char router_2_address[1024];
@@ -98,6 +99,7 @@ int main(int argc, char *argv[])
         {
             cost_array[i][j]=10000;
         }
+        cost_array[i][i]=0;
     }
 
 
@@ -201,10 +203,30 @@ int main(int argc, char *argv[])
             printf("%d\n", *buff);
             temp_cost=*buff;
             printf("%d\n", temp_cost);
+            string go;
+            if(string(router_1_address)==self_ip)
+            {
+                go=string(router_2_address);
+            }
+            else
+            {
+                go=string(router_1_address);
+            }
+            cout<<"go "<<go<<endl;
+            for(int i=0;i<neighbours.size();i++)
+            {
+                if(neighbours[i].dest==go)
+                {
+                    neighbours[i].cost=temp_cost;
+                }
+
+            }
+
+
 
 
         }
-        else if(action=="frwd")
+        else if(action=="frwd")         ///SEMI TESTED
         {
             in_addr r1,r2;
             buff=static_cast<int *>(static_cast<void *>(receive_buffer+4));
@@ -213,17 +235,48 @@ int main(int argc, char *argv[])
             strcpy(router_1_address,pointing);
             //printf("%s\n", router_1_address);
             printf("%s\n", router_1_address);
-            sh=static_cast<short *>(static_cast<void *>(receive_buffer+12));
+            sh=static_cast<short *>(static_cast<void *>(receive_buffer+8));
             //printf("%d\n", *buff);
             temp_cost=*sh;
             printf("%dH\n", temp_cost);
-            buff=static_cast<int *>(static_cast<void *>(receive_buffer+14));
+            buff=static_cast<int *>(static_cast<void *>(receive_buffer+10));
             pointing=(char *)buff;
             strcpy(message,pointing);
-            printf("%s\n", message); 
+            printf("%s\n", message);
+            string go,msgstr;
+            go=string(router_1_address);
+            msgstr=string(message);
+            if(go==self_ip)
+            {
+                cout<<msgstr<<" packet reached destination"<<endl;
+            }
+            else
+            {
+                for(int i=0;i<routing_table.size();i++)
+                {
+                    if(routing_table[i].dest==go)
+                    {
+                        char msgchar[1024];
+                        memcpy(msgchar,"frwd",4);
+                        char ipad[1024];
+                        strcpy(ipad,routing_table[i].nexthop.c_str());
+                        printf("%s\n", ipad);
+                        temporary_socket.sin_addr.s_addr=inet_addr(ipad);
+                        memcpy(msgchar+4,&temporary_socket.sin_addr.s_addr,4);
+                        memcpy(msgchar+8,sh,2);
+                        memcpy(msgchar+10,message,temp_cost);
+                        temporary_socket.sin_port=htons(4747);
+                        temporary_socket.sin_family=AF_INET;
+                        sendto(sockfd,msgchar,1024,0,(struct sockaddr*) &temporary_socket,sizeof(sockaddr_in));
+                        cout<<msgstr<<" packet forwarded to "<<routing_table[i].nexthop<<endl;
+
+                    }
+                }
+            }
+
 
         }
-        else if(action=="send")
+        else if(action=="send") //TESTED
         {
             in_addr r1,r2;
             buff=static_cast<int *>(static_cast<void *>(receive_buffer+4));
@@ -248,6 +301,32 @@ int main(int argc, char *argv[])
             printf("%s\n", message); 
             /* -----------------------
             */
+            string go,msgstr;
+            go=string(router_2_address);
+            msgstr=string(message);
+            for(int i=0;i<routing_table.size();i++)
+            {
+                if(routing_table[i].dest==go)
+                {
+                        char msgchar[1024];
+                        memcpy(msgchar,"frwd",4);
+                        char ipad[1024];
+                        strcpy(ipad,routing_table[i].nexthop.c_str());
+                        printf("ipad %s\n", ipad);
+                        temporary_socket.sin_addr.s_addr=inet_addr(ipad);
+                        memcpy(msgchar+4,&temporary_socket.sin_addr.s_addr,4);
+                        memcpy(msgchar+8,sh,2);
+                        memcpy(msgchar+10,message,temp_cost);
+                        temporary_socket.sin_port=htons(4747);
+                        temporary_socket.sin_family=AF_INET;
+                        sendto(sockfd,msgchar,1024,0,(struct sockaddr*) &temporary_socket,sizeof(sockaddr_in));
+                        cout<<msgstr<<" packet forwarded to "<<routing_table[i].nexthop<<endl;
+
+                }
+            }
+
+
+
 
         }
         else if(action=="rout")
@@ -268,8 +347,17 @@ int main(int argc, char *argv[])
                 printf("%d\n", temp_cost);
                 int y=getid(string(router_1_address));
                 cost_array[x][y]=temp_cost;
-
             }
+            printf("JUST RECEIVED PACKET FROM %d. ME %d\n", x,self_id);
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=n;j++)
+                {
+                    printf("%d ", cost_array[i][j]);
+                }
+                printf("\n");
+            }
+
 
         }
         else if(action=="clk ")
@@ -289,7 +377,7 @@ int main(int argc, char *argv[])
             clockno++;
             printf("%d\n", clockno);
             printf("Khelbo\n");
-            for(int i=0;i<neighbours.size();i++)
+/*            for(int i=0;i<neighbours.size();i++)
             {
                 int idx=getid(neighbours[i].dest);
                 printf("%d\n", clocklast[idx]);
@@ -300,10 +388,12 @@ int main(int argc, char *argv[])
                 }
 
             }
-
+*/
+            /*
             for(int i=1;i<=n;i++)
             {
                 cost_array[self_id][i]=10000;
+                int maxv=10000;
                 for(int j=0;j<neighbours.size();j++)
                 {
                     int v=getid(neighbours[j].dest);
@@ -311,7 +401,35 @@ int main(int argc, char *argv[])
                     cost_array[self_id][i]=min(neighbours[j].cost+cost_array[v][i],cost_array[self_id][i]);
                 }
             }
+            */
+            for(int i=0;i<neighbours.size();i++)
+            {
+                cout<<"neigh "<<neighbours[i].dest<<neighbours[i].cost<<endl;
+            }
+            for(int i=0;i<routing_table.size();i++)
+            {
 
+                int maxv=10000;
+                int x=self_id;
+                string nh="";
+                int y=getid(routing_table[i].dest);
+                for(int j=0;j<neighbours.size();j++)
+                {
+                    int v=getid(neighbours[j].dest);
+                    if((neighbours[j].cost + cost_array[v][y]) < maxv)
+                    {
+                        cout<<"j "<<j<<v<<i<<endl;
+                        maxv=neighbours[j].cost+cost_array[v][y];
+                        nh=neighbours[j].dest;
+                        routing_table[i].nexthop=nh;
+                        routing_table[i].cost=maxv;
+                    }
+
+                }
+
+            }
+            print_table();
+            printf("--------------------------------\n");
             for(int i=1;i<=n;i++)
             {
                 for(int j=1;j<=n;j++)
@@ -320,8 +438,18 @@ int main(int argc, char *argv[])
                 }
                 printf("\n");
             }
+            printf("--------------------------------\n");
 
             /*------------------------------------------------ YET WORK BAKI FOR SENDING TABLE*/
+
+            char msgchar[2048];
+            for(int i=0;i<routing_table.size();i++)
+            {
+                
+            }
+
+
+
         }
         
     }
